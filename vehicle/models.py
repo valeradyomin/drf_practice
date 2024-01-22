34f21bffd99c96e18e8c234dbe63e13a332160e9
Valera <wahrwolffe@gmail.com>
2024-01-23 01:20:19 +0300

    add Car

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'машина'
+        verbose_name_plural = 'машины'
