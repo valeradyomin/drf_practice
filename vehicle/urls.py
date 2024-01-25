c706077050370f991b9f4b6106c6d75129ca2799
Valera <wahrwolffe@gmail.com>
2024-01-26 01:59:52 +0300

    7. Практика. Вывод вложенных объектов

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,7 +4,7 @@ from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
 from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
-    MotoDestroyAPIView, MilageCreateAPIView
+    MotoDestroyAPIView, MilageCreateAPIView, MotoMilageListAPIView
 
 app_name = VehicleConfig.name
 
@@ -19,4 +19,5 @@ urlpatterns = [
     path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto_delete'),
 
     path('milage/create/', MilageCreateAPIView.as_view(), name='milage_create'),
+    path('moto/milage/', MotoMilageListAPIView.as_view(), name='moto_milage_list'),
 ] + router.urls
