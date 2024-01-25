3fb63df8154cbd06d3c1a0eb4fbf30ec751adf19
Valera <wahrwolffe@gmail.com>
2024-01-25 03:08:09 +0300

    last_milage / get_last_milage

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -4,15 +4,25 @@ from vehicle.models import Car, Moto, Milage
 
 
 class CarSerializer(serializers.ModelSerializer):
+    last_milage = serializers.IntegerField(source='milage_set.all.first.milage')
+
     class Meta:
         model = Car
         fields = '__all__'
 
 
 class MotoSerializer(serializers.ModelSerializer):
+    last_milage = serializers.SerializerMethodField()
+
     class Meta:
         model = Moto
         fields = '__all__'
+
+    def get_last_milage(self, obj):
+        if obj.milage_set.all().first():
+            return obj.milage_set.all().first().milage
+
+        return 0
 
 
 class MilageSerializer(serializers.ModelSerializer):
