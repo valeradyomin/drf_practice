1bf4c63f5e18e76b429ed3155ea33811bef898ec
Valera <wahrwolffe@gmail.com>
2024-02-08 01:40:34 +0300

    25.1 Права доступа в DRF 5 Практика. Права на контроллер

--- config/settings.py
+++ config/settings.py
@@ -152,5 +152,9 @@ LOGOUT_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+            # 'rest_framework.permissions.IsAuthenticated',
+            'rest_framework.permissions.AllowAny',
+    ]
 }
