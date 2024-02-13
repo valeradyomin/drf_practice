273c2b8e4f7ee1907d0adb2d88e9155f22559033
Valera <wahrwolffe@gmail.com>
2024-02-14 01:54:56 +0300

    25.2 Валидаторы, пагинация и тесты done

--- vehicle/tests.py
+++ vehicle/tests.py
@@ -1,12 +1,19 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from vehicle.models import Car
+
 
 class VehicleTestCase(APITestCase):
     def setUp(self):
         pass
 
     def test_create_car(self):
+        """
+        Тест создания автомобиля путем отправки POST-запроса на конечную точку '/cars/' с указанными данными,
+        а затем проверка кода состояния ответа, JSON-данных и существования созданного объекта Car.
+        """
+
         data = {
             'title': 'Car',
             'description': 'Car description',
@@ -16,3 +23,26 @@ class VehicleTestCase(APITestCase):
         print(response.json())
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(
+            response.json(),
+            {'id': 1, 'milage': [], 'title': 'Car', 'description': 'Car description', 'owner': None}
+        )
+
+        self.assertTrue(Car.objects.exists())
+
+    def test_list_cars(self):
+        """
+        Тест получения списка автомобилей путем отправки GET-запроса на конечную точку '/cars/'.
+        """
+
+        Car.objects.create(title='Car list', description='Car description list')
+
+        response = self.client.get('/cars/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(
+            response.json(),
+            [{'id': 2, 'milage': [], 'title': 'Car list', 'description': 'Car description list', 'owner': None}]
+        )
