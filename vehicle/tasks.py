53bc667691b317849374f464efaa7010447eb643
Valera <wahrwolffe@gmail.com>
2024-02-27 03:17:24 +0300

    26.2. Celery setup add tasks

---
+++ vehicle/tasks.py
@@ -0,0 +1,13 @@
+from vehicle.models import Car, Moto
+
+
+def check_milage(pk, model):
+    if model == 'Car':
+        instance = Car.objects.filter(pk=pk).first()
+    else:
+        instance = Moto.objects.filter(pk=pk).first()
+
+    if instance:
+        previous_milage = -1
+
+        
