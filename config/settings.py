e969af5b253f09cdb4654efe5d686cd58c49c06b
Valera <wahrwolffe@gmail.com>
2024-03-01 00:20:20 +0300

    26.2. Celery 7. Практика. Периодические задачи +

--- config/settings.py
+++ config/settings.py
@@ -196,3 +196,11 @@ CELERY_TIMEZONE = 'Europe/Moscow'
 
 # Максимальное время на выполнение задачи
 # CELERY_TASK_TIME_LIMIT = 30 * 60
+
+# Настройки для Celery, чтобы использовать периодические задачи
+# CELERY_BEAT_SCHEDULE = {
+#     'task-name': {
+#         'task': 'myapp.tasks.my_task',  # Путь к задаче
+#         'schedule': timedelta(minutes=10),  # Расписание выполнения задачи (например, каждые 10 минут)
+#     },
+# }
