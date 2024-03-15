b36c5d8baae4300a948eea921683237b247a15cd
Valera Demin <wahrwolffe@gmail.com>
2024-03-16 00:34:08 +0300

    27.2. Docker Compose

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
 # Используем базовый образ Python
-FROM python:3.10
+FROM python:3
 
 # Устанавливаем рабочую директорию в контейнере
 WORKDIR /app
 
 # Копируем requirements.txt в рабочую директорию контейнера
-COPY requirements.txt .
+COPY ./requirements.txt /app
 
 # Устанавливаем зависимости
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
 # Копируем код приложения в рабочую директорию контейнера
-COPY . .
-
-## Команда для запуска приложения при старте контейнера
-#CMD ["python", "manage.py", "runserver", "127.0.0.1:8000"]
+COPY . /app
--- config/settings.py
+++ config/settings.py
@@ -86,14 +86,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASS'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
+# Postgres settings for docker
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
+        # 'PORT': os.getenv('DB_PORT'),
     }
 }
 
